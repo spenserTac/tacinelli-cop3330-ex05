2d5546bd76005eabd9d19213dc4bfeb6fa92e49e
spenserTac <spenserdt@gmail.com>
2021-08-24 21:25:08 -0400

    Exercise Finished

---
+++ src/main/java/Exercise_5.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Exercise_5{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("What is the first number? ");
+        String temp = scanner.nextLine();
+        int f_num = Integer.parseInt(temp);
+
+        System.out.print("What is the second number? ");
+        String temp2 = scanner.nextLine();
+        int s_num = Integer.parseInt(temp2);
+
+        System.out.println(f_num + " + " + s_num + " = " + (f_num + s_num) + "\n" +
+        f_num + " - " + s_num + " = " + (f_num - s_num) + "\n" +
+        f_num + " * " + s_num + " = " + (f_num * s_num) + "\n" +
+        f_num + " / " + s_num + " = " + (f_num / s_num));
+    }
+}
